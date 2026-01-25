45dc13bbd5457b502abf23b4d18f6f628d7ac844
jeanluckawel <20ka208@esisalama.org>
2026-01-25 16:46:21 +0200

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	resources/views/Employee/edit.blade.php
    #	resources/views/Employee/list.blade.php
    #	routes/web.php

---
+++ database/migrations/2026_01_25_133926_create_customers_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('customers', function (Blueprint $table) {
+            $table->ulid('id')->primary();
+            $table->string('name');
+            $table->string('id_nat')->nullable();
+            $table->string('rccm')->nullable();
+            $table->string('nif')->nullable();
+            $table->string('province')->nullable();
+            $table->string('ville')->nullable();
+            $table->string('commune')->nullable();
+            $table->string('quartier')->nullable();
+            $table->string('avenue')->nullable();
+            $table->string('numero')->nullable();
+            $table->string('telephone')->nullable();
+            $table->string('email')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('customers');
+    }
+};
